04d9edc4131393048a62a84afee69d0b4e1be874
배수한 <qotngks152@naver.com>
2022-10-14 15:18:41 +0900

    init commit

---
+++ src/main/java/com/nhnacademy/edu/springframework/project/BootStrap.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.edu.springframework.project;
+
+public class BootStrap {
+    public static void main(String[] args) {
+        System.out.println("BootStrap.main");
+    }
+}
